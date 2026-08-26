04b5d7ea46f1880527cdbf3d4bde23d7c1501009
Parhamyaa <surfadventure26@gmail.com>
2026-08-26 18:57:29 +0200

    Update discord.js

--- api/auth/discord.js
+++ api/auth/discord.js
@@ -1,1 +1,25 @@
+export default function handler(req, res) {
+    const clientId = process.env.DISCORD_CLIENT_ID;
 
+    if (!clientId) {
+        return res.status(500).send("DISCORD_CLIENT_ID fehlt.");
+    }
+
+    const redirectUri = process.env.DISCORD_REDIRECT_URI;
+
+    if (!redirectUri) {
+        return res.status(500).send("DISCORD_REDIRECT_URI fehlt.");
+    }
+
+    const params = new URLSearchParams({
+        client_id: clientId,
+        response_type: "code",
+        redirect_uri: redirectUri,
+        scope: "identify guilds"
+    });
+
+    const discordUrl =
+        "https://discord.com/oauth2/authorize?" + params.toString();
+
+    res.redirect(302, discordUrl);
+}
