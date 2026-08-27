0b996d9fce12f38e0b5ee49356e113e7d2a3640b
Parhamyaa <surfadventure26@gmail.com>
2026-08-27 15:15:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,13 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`Bot ist online als ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
